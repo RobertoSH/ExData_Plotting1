63810272df0f97649de4b3f0290defc8aa123e78
RobertoSH <rsuarez@rsuarez.netintellego.com>
2014-05-08 03:12:55 -0500

    final version

--- plot3.R
+++ plot3.R
@@ -1,20 +1,27 @@
 # Begins Plot 3
 
+# Read data at home folder
 x<-read.table(file='~/household_power_consumption.txt',sep=";",header=T,
               stringsAsFactors=F)
+# Change strings by numeric
 x[,3:8]<-apply(x[,3:8],2,as.numeric)
+# Change string by date
 x$Date<-as.Date(x$Date,format="%d/%m/%Y")
-
+#Extract only required information
 y<-x[x$Date>=as.Date('2007-02-01') & x$Date<=as.Date('2007-02-02'),]
+#Remove original table
 rm(x)
 
 #install.packages('plyr')
 library(plyr)
+#Extract weekdays
 y$w<-weekdays(y$Date)
+#Required agregates
 Freq1<-ddply(y,~w+Time,function(x) sum(x$Sub_metering_1))$V1
 Freq2<-ddply(y,~w+Time,function(x) sum(x$Sub_metering_2))$V1
 Freq3<-ddply(y,~w+Time,function(x) sum(x$Sub_metering_3))$V1
 
+#Begin png
 png(filename="~/github/ExDataPlotting1/Plot3.png", width = 480, height = 480,
     units = "px")
 plot(Freq1,type='l',xlab='',ylab='Energy sub metering',xaxt='n')
